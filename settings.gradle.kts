80df1bc628c58e4fce549b8513ec288c53660685
lukas.mrtnz <lukas.mrtnz@eduge.ch>
2023-11-17 08:32:52 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "App with text composables"
+include(":app")
+include(":app")
+ 
